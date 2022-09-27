04a48e00b17ef979bce778ae65c2ee60794fed00
alexis90210 <alexisng90210@gmail.com>
2022-09-27 15:41:25 +0100

    create view and config

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,11 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.get('/', async ({ view }) => {
+  return view.render('home')
 })
+
+
+Route.get('/eleves', 'ElevesController.index')
+
+
